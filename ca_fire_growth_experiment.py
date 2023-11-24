730e9441b190887d7d98cf13bcbaf414932e6af1
tg.veale@gmail.com <tg.veale@gmail.com>
2023-11-24 10:21:31 +0000

    corrected author

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  4 20:35:51 2022
+Created on Fri Nov  24 10:18:30 2022
 
-@author: steph
+@author: tom
 """
 from ca_fire import evolve_forest, write_netcdf
 
